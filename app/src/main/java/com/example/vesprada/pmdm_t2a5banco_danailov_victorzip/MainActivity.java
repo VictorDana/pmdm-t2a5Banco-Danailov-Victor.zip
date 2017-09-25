e3f338f0e4a8d188441f64f298d2584e4256b877
victor <viktordanailov@gmail.com>
2017-09-25 17:10:32 +0200

    prueba

---
+++ app/src/main/java/com/example/vesprada/pmdm_t2a5banco_danailov_victorzip/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vesprada.pmdm_t2a5banco_danailov_victorzip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
